8ec3805cda81bd3263077fc4dab42b1996206be4
Alexander Ho <alexyho9@gmail.com>
2023-12-15 13:41:38 -0800

    completed depth first values recursive function

--- binary-trees/FCC-binary-trees.js
+++ binary-trees/FCC-binary-trees.js
@@ -8,6 +8,7 @@ class Node {
     }
 }
 
+// Set up Tree
 const a = new Node('a');
 const b = new Node('b');
 const c = new Node('c');
@@ -20,3 +21,36 @@ a.right = c;
 b.left = d;
 b.right = e;
 c.right = f;
+
+
+// Depth First Traversal
+const iDFV = (root) => {
+    if (!root) return [];
+    const result = [];
+    const stack = [ root ];
+    while (stack.length > 0) {
+        const current = stack.pop();
+        result.push(current.value);
+
+        if (current.right) stack.push(current.right);
+        if (current.left) stack.push(current.left);
+    }
+    return result;
+}
+
+// Recursive DFS
+const rDFV = (root) => {
+    if (!root) return [];
+    const leftValues = rDFS(root.left);
+    const rightValues = rDFS(root.right);
+    return [ root.value, ...leftValues, ...rightValues ];
+}
+
+// console.log(iDFV(a));
+// console.log(rDFV(a));
+
+
+// Iterative Breadth First Traversal
+
+
+// Recursive Breadth First Traversal
